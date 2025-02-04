ddd7eea49e9603d9d3799839882367b46de01b53
Errol-Hassall <Errol.Hassall99@gmail.com>
2025-02-05 09:35:36 +1100

    added health check endpoint

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ This is a repo for me to learn a bit about Rust in a web dev capacity
 
 ## TODO
 
+- [ ] Healthcheck endpoint
 - [ ] Fetch todays weather forcast for ballarat
 - [ ] Send a message via telgram
 - [ ] Wait until a specific time of day to fetch the information then send it otherwise wait
 - [ ] Send that forcast to Telagram or Signal each morning
 - [ ] Endpoint that you can pass a location and it returns the forcast
+- [ ] Docker and docker compose
+- [ ] Host on my own server
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("Alive")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(health)
     })
     .bind(("127.0.0.1", 4000))?
     .run()
