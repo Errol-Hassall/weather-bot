15b872cfc13f87709e8da79bb5d526faa7cf3bdd
Errol-Hassall <Errol.Hassall99@gmail.com>
2025-02-05 12:13:45 +1100

    Sending the min and max temps for the current day

--- src/weather.rs
+++ src/weather.rs
@@ -1,18 +1,47 @@
 use actix_web::{get, HttpResponse, Responder};
-use teloxide::{prelude::Requester, requests::JsonRequest, Bot};
+use serde::Deserialize;
+use teloxide::{prelude::Requester, Bot};
+
+#[derive(Debug, Clone, Deserialize)]
+struct Daily {
+    time: Vec<String>,
+    temperature_2m_max: Vec<f32>,
+    temperature_2m_min: Vec<f32>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct WeatherForcast {
+    daily: Daily,
+}
 
 #[get("/weather/weather-forcast")]
 pub async fn weather_forcast() -> impl Responder {
-    let response = reqwest::get("https://api.open-meteo.com/v1/forecast?latitude=-37.5662&longitude=143.8496&daily=temperature_2m_max,temperature_2m_min&timezone=Australia%2FSydney&forecast_days=1").await.unwrap().text().await.unwrap();
+    let url = String::from("https://api.open-meteo.com/v1/forecast?latitude=-37.5662&longitude=143.8496&daily=temperature_2m_max,temperature_2m_min&timezone=Australia%2FSydney&forecast_days=1");
+    let response: WeatherForcast = reqwest::get(url)
+        .await
+        .unwrap()
+        .json::<WeatherForcast>()
+        .await
+        .unwrap();
 
     let _ = send_bot_message(response.clone()).await;
 
-    HttpResponse::Ok().body(response)
+    HttpResponse::Ok().body(String::from(&response.daily.time[0]))
 }
 
-async fn send_bot_message(_weather: String) -> JsonRequest<teloxide::payloads::SendMessage> {
+async fn send_bot_message(weather: WeatherForcast) -> teloxide::prelude::Message {
     let channel_id = dotenv::var("CHANNEL_ID").unwrap();
     let bot = Bot::from_env();
 
-    bot.send_message(String::from(channel_id), "Test")
+    let min = &weather.daily.temperature_2m_min[0];
+    let max = &weather.daily.temperature_2m_max[0];
+
+    let message = format!("Minimum temperature of {min} and a maximum of {max}");
+
+    let response = bot
+        .send_message(String::from(channel_id), message)
+        .await
+        .unwrap();
+
+    response
 }
